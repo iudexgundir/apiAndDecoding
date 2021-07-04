d397cddefb350b47b844d250923f1e62780ca65e
Govorov Erkhaan <govoroverx@gmail.com>
2021-07-04 07:09:20 +0300

    Initial Commit

---
+++ apiAndDecoding/apiAndDecodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiAndDecodingApp.swift
+//  apiAndDecoding
+//
+//  Created by Govorov Erkhaan on 04.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct apiAndDecodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
